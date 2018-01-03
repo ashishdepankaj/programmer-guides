fe0dc7f8ab0b21506e0dd89ae7cab7bdabc3b073
ashishdepankaj <ashishdepankaj@gmail.com>
2018-01-02 22:57:24 -0500

    Added EmailUtils class

---
+++ src/main/java/org/depankaj/utils/EmailUtils.java
@@ -0,0 +1,13 @@
+package org.depankaj.utils;
+
+import java.util.Collection;
+
+public final class EmailUtils {
+
+    private EmailUtils() {} //To prevent user instantiation
+
+    public static void mail(String subject, String body, Collection<String> to, Collection<String> cc, Collection<String> bcc) {
+        //Send email here
+
+    }
+}
